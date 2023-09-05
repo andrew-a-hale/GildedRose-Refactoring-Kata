0b12328acfaed48101600a6c9a3879c81cf87106
Andrew Hale <andrewhale@192-168-1-4.tpgi.com.au>
2023-09-05 16:19:11 +1000

    python-oo update

---
+++ python-oo/gilded_rose.py
@@ -0,0 +1,88 @@
+from typing import List, Protocol
+from item import Item
+
+
+def increment_quality(item: Item, n: int = 1, max: int = 50) -> None:
+    item.quality = min(item.quality + n, max)
+
+
+def decrement_quality(item: Item, n: int = 1, min: int = 0) -> None:
+    item.quality = max(item.quality - n, min)
+    if item.sell_in < 0:
+        item.quality = max(item.quality - n, min)
+
+
+def decrement_sell_in(item: Item, n: int = 1) -> None:
+    item.sell_in -= n
+
+
+class ItemUpdater(Protocol):
+    def update_sell_in(item: Item) -> None:
+        ...
+
+    def update_quality(item: Item) -> None:
+        ...
+
+
+class DefaultUpdater(ItemUpdater):
+    def update_sell_in(item: Item) -> None:
+        decrement_sell_in(item)
+
+    def update_quality(item: Item) -> None:
+        decrement_quality(item)
+
+
+class LegendaryUpdater(DefaultUpdater):
+    def update_sell_in(item: Item) -> None:
+        pass
+
+    def update_quality(item: Item) -> None:
+        pass
+
+
+class AgedUpdater(DefaultUpdater):
+    def update_quality(item: Item) -> None:
+        increment_quality(item)
+        if item.sell_in < 10:
+            increment_quality(item)
+        if item.sell_in < 5:
+            increment_quality(item)
+
+
+class PerishableUpdater(DefaultUpdater):
+    def update_quality(item: Item) -> None:
+        increment_quality(item)
+        if item.sell_in < 10:
+            increment_quality(item)
+        if item.sell_in < 5:
+            increment_quality(item)
+        if item.sell_in < 0:
+            item.quality = 0
+
+
+class ConjuredUpdater(DefaultUpdater):
+    def update_quality(item: Item) -> None:
+        decrement_quality(item, 2)
+
+
+AGED_BRIE = "Aged Brie"
+BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+CONJURED_MANA_CAKE = "Conjured Mana Cake"
+ITEM_UPDATERS = {
+    AGED_BRIE: AgedUpdater,
+    BACKSTAGE_PASS: PerishableUpdater,
+    SULFURAS: LegendaryUpdater,
+    CONJURED_MANA_CAKE: ConjuredUpdater,
+}
+
+
+def update_quality(items: List[Item]):
+    for item in items:
+        update_quality_single(item)
+
+
+def update_quality_single(item: Item):
+    item_updater = ITEM_UPDATERS.get(item.name, DefaultUpdater)
+    item_updater.update_sell_in(item)
+    item_updater.update_quality(item)
---
+++ python-oo/test_gilded_rose.py
@@ -0,0 +1,80 @@
+from gilded_rose import (
+    AGED_BRIE,
+    SULFURAS,
+    BACKSTAGE_PASS,
+    CONJURED_MANA_CAKE,
+    update_quality_single,
+)
+from item import Item
+
+
+def test_foo():
+    item = Item("foo", 0, 0)
+    update_quality_single(item)
+    assert "foo" == item.name
+
+
+def test_past_sell_by_date():
+    item = Item("foo", 6, -1)
+    update_quality_single(item)
+    assert item.quality == 4
+
+
+def test_quality_never_negative():
+    item = Item("foo", 0, 0)
+    update_quality_single(item)
+    assert item.quality == 0
+
+
+def test_aged_brie_quality_increase_over_time():
+    item = Item(AGED_BRIE, 3, 0)
+    update_quality_single(item)
+    assert item.quality == 5
+
+
+def test_max_quality_is_50():
+    item = Item(AGED_BRIE, 50, 0)
+    update_quality_single(item)
+    assert item.quality == 50
+
+
+def test_sulfuras_doesnt_decrease_in_quality():
+    item = Item(SULFURAS, 3, 0)
+    update_quality_single(item)
+    assert item.quality == 3
+
+
+def test_backstage_pass_sellin_greater_than_10():
+    item = Item(BACKSTAGE_PASS, 3, 11)
+    update_quality_single(item)
+    assert item.quality == 4
+
+
+def test_backstage_pass_sellin_less_than_10():
+    item = Item(BACKSTAGE_PASS, 3, 10)
+    update_quality_single(item)
+    assert item.quality == 5
+
+
+def test_backstage_pass_sellin_less_than_5():
+    item = Item(BACKSTAGE_PASS, 3, 5)
+    update_quality_single(item)
+    assert item.quality == 6
+
+
+def test_backstage_pass_sellin_less_than_0():
+    item = Item(BACKSTAGE_PASS, 3, -1)
+    update_quality_single(item)
+    assert item.quality == 0
+
+
+def test_conjured_items_degrade_twice_as_fast_negative_sellin():
+    item = Item(CONJURED_MANA_CAKE, 6, -1)
+    update_quality_single(item)
+    assert item.quality == 2
+
+
+def test_conjured_items_degrade_twice_as_fast():
+    item = Item(CONJURED_MANA_CAKE, 6, 1)
+    update_quality_single(item)
+    assert item.quality == 4
